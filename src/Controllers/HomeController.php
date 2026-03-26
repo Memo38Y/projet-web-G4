73d8f03f1d8962ffc17dbc7b7d840c5d76ef0071
Eim <eimeo.abihssira.lab@gmail.com>
2026-03-26 11:20:07 +0100

    feat: update homepage to retrieve and display all offers with enhanced statistics layout

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -16,8 +16,8 @@ class HomeController
 
     public function index()
     {
-        // 1. On récupère les offres pour l'affichage classique
-        $dernieresOffres = \App\Models\Offre::getAll(); 
+        // 1. On récupère TOUTES les offres
+        $offres = \App\Models\Offre::getAll(); 
 
         // 2. Gestion des favoris pour l'affichage des drapeaux bleus
         $mesFavorisIds = [];
@@ -26,14 +26,14 @@ class HomeController
             $mesFavorisIds = array_column($offresFavorites, 'Id_OFFRE');
         }
 
-        // 3. NOUVEAU : On récupère les statistiques en temps réel !
+        // 3. On récupère les statistiques en temps réel
         $stats = \App\Models\Stats::getDashboardStats();
 
-        // 4. On envoie tout à la vue
+        // 4. On envoie tout à la vue (on utilise 'offres' au lieu de 'dernieresOffres')
         echo $this->twig->render('accueil.html.twig', [
-            'dernieresOffres' => $dernieresOffres,
+            'offres' => $offres,
             'mesFavorisIds' => $mesFavorisIds,
-            'stats' => $stats // On passe le tableau de stats à Twig
+            'stats' => $stats
         ]);
     }
 }
